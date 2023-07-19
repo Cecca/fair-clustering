443263d541d3d50926f6a75761032849dae6896e
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2023-07-19 13:55:40 +0200

    update experiments configuration

--- kcenter.py
+++ kcenter.py
@@ -282,7 +282,7 @@ if __name__ == "__main__":
         algo.fit_predict(data, colors, fairness_constraints)
         logging.basicConfig(level=logging.INFO)
 
-    # warmup()
+    warmup()
 
     k = 32
     delta = 0.0
