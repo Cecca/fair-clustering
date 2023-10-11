0ae10ddf9a88239da93ea8fc014b9c48f65c055d
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2023-10-11 12:05:51 +0200

    update experiments

--- batch.sh
+++ batch.sh
@@ -2,20 +2,20 @@
 #SBATCH --mem 0
 #SBATCH --exclusive
 
+#apptainer exec \
+#	--bind $(pwd):/work \
+#	--pwd /work \
+#	fairclustering.sif \
+#	python results.py results.hdf5
+#
+#apptainer exec \
+#	--bind $(pwd):/work \
+#	--pwd /work \
+#	fairclustering.sif \
+#	python datasets.py
+#
 apptainer exec \
 	--bind $(pwd):/work \
 	--pwd /work \
 	fairclustering.sif \
-	python results.py results.hdf5
-
-apptainer exec \
-	--bind $(pwd):/work \
-	--pwd /work \
-	fairclustering.sif \
-	python datasets.py
-
-apptainer exec \
-	--bind $(pwd):/work \
-	--pwd /work \
-	fairclustering.sif \
-	python experiments.py ~/opt/cplex/cplex/bin/x86-64_linux/cplex
+	python experiments.py ~/opt/cplex/cplex/bin/x86-64_linux/cplex $@
--- experiments.py
+++ experiments.py
@@ -10,7 +10,7 @@ import logging
 import signal
 from baseline.adapter import KFC
 
-TIMEOUT_SECS = 12*60*60 # 12 hours
+TIMEOUT_SECS = 1*60*60 # 1 hour
 
 
 class TimeoutException(Exception):
@@ -66,25 +66,25 @@ def delta_influence():
 
 def exhaustive():
     ofile = "results.hdf5"
-    ks = [32, 64]
-    # ks = [2, 4, 8, 16, 32]
+    ks = [4, 8, 16, 32, 64]
     deltas = [0.01]
     all_datasets = datasets.datasets()
-    all_datasets = ["athlete", "diabetes"]
+    all_datasets = [d for d in all_datasets if d not in ["census1990_age", "hmda", "census"]]
+    #all_datasets = ["athlete"]
     for dataset, delta, k in itertools.product(all_datasets, deltas, ks):
         n, dim = datasets.dataset_size(dataset)
         algos = [
-            kcenter.Dummy(k),
-            kcenter.UnfairKCenter(k),
+            #kcenter.Dummy(k),
+            #kcenter.UnfairKCenter(k),
             kcenter.BeraEtAlKCenter(k, cplex_path),
-            KFC(k, cplex_path)
+            #KFC(k, cplex_path)
         ] + [
-            kcenter.CoresetFairKCenter(
-                k, tau, cplex_path, seed=seed)
+            #kcenter.CoresetFairKCenter(
+            #    k, tau, cplex_path, seed=seed)
             #for tau in [2*k, 8*k, 32*k, 64*k, 128*k, 256*k]
-            for tau in [32*k]
-            for seed in [1,2,3,4,5,6,7]
-            if tau <= n
+            #for tau in [k, 32*k]
+            #for seed in [1,2,3,4,5,6,7]
+            #if tau <= n
         ]
         for algo in algos:
             evaluate(dataset, delta, algo, k, ofile)
@@ -94,18 +94,18 @@ def mr_experiments():
     ofile = "results.hdf5"
     ks = [32]
     deltas = [0.01]
-    all_datasets = ["diabetes", "athlete", "census1990", "hmda"]
+    all_datasets = ["census1990_age", "hmda"]
     for dataset, delta, k in itertools.product(all_datasets, deltas, ks):
         n, dim = datasets.dataset_size(dataset)
-        for threads in [2, 4, 8, 16]:
-            for shuffle_seed in [11234,1234,4234,1234,432]:#,1,2,3,4,5]:
+        for threads in [4]:# [2, 4, 8, 16]:
+            for shuffle_seed in [11234,1234,4234,1234,432,1,2,3,4,5,6,7,8,9,10,11]:
                 algos = [
                     mapreduce.BeraEtAlMRFairKCenter(
                         k, threads, cplex_path, seed=shuffle_seed)
                 ] + [
                     mapreduce.MRCoresetFairKCenter(
                         k, tau, threads, cplex_path, seed=shuffle_seed)
-                    for tau in [2*k, 4*k, 8*k, 16*k]
+                    for tau in [2*k]
                     if tau <= n
                 ]
                 for algo in algos:
@@ -116,20 +116,21 @@ def streaming_experiments():
     ofile = "results.hdf5"
     ks = [32]
     deltas = [0.01]
-    all_datasets = ["diabetes", "athlete", "census1990", "hmda"]
+    all_datasets = ["census1990_age", "hmda"]
+    #all_datasets = ["census1990_age"]
     #all_datasets = datasets.datasets()
     for dataset, delta, k in itertools.product(all_datasets, deltas, ks):
-        for shuffle_seed in [1,2,3,4,5,6,7,8,9,10,11,12]:
+        for shuffle_seed in range(16): #:[1,2,3,4,5,6,7,8,9,10,11,12]:
             n, dim = datasets.dataset_size(dataset)
             algos = [
                 streaming.BeraEtAlStreamingFairKCenter(
                     k, epsilon, cplex_path, seed=shuffle_seed)
-                for epsilon in [0.5, 0.1, 0.05, 0.01]
+                for epsilon in [0.1, 0.05, 0.01, 0.005, 0.001]
+                #for epsilon in [0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0002]
             ] + [
                 streaming.StreamingCoresetFairKCenter(
                     k, k*tau, cplex_path, seed=shuffle_seed)
-                for tau in [128, 256, 512]
-                #for tau in [2, 4, 8, 32, 128, 512]
+                for tau in [2, 4, 8, 32, 128, 512, 1024]
                 if tau <= n
             ]
             for algo in algos:
@@ -140,13 +141,19 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     signal.signal(signal.SIGALRM, timeout_handler)
 
-    if len(sys.argv) == 2:
-        cplex_path = sys.argv[1]
-    else:
-        cplex_path = None
+    if len(sys.argv) != 3:
+        print("You should provide the path to CPLEX and the name of the bench to run")
+        sys.exit(1)
 
-    #exhaustive()
-    #delta_influence()
-    #mr_experiments()
-    streaming_experiments()
+    cplex_path = sys.argv[1]
+    bench_name = sys.argv[2]
+
+    benches = {
+        "exhaustive": exhaustive,
+        "delta_influence": delta_influence,
+        "mapreduce": mr_experiments,
+        "streaming": streaming_experiments,
+    }
+
+    benches[bench_name]()
 
